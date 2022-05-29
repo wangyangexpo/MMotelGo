c569b5ba1bc1c2dd0b1443c22bb4aafe0d36bdf7
wy187819 <wy187819@alibaba-inc.com>
2022-05-29 21:41:25 +0800

    feat: 门店管理

--- src/pages/pms/Setting/ShopManage/index.tsx
+++ src/pages/pms/Setting/ShopManage/index.tsx
@@ -1,31 +1,52 @@
-import React from 'react';
-import { Descriptions, Button } from 'antd';
+import React, { useState } from 'react';
+import { Descriptions, Button, Form, Input } from 'antd';
 import './style.less';
 
+const FormItem = Form.Item;
+const DescItem = Descriptions.Item;
+
 const ShopMange: React.FC = () => {
+  const [editable, setEditable] = useState(false);
+
   return (
-    <div className="shop-manage-container">
+    <Form className="shop-manage-container">
       <Descriptions
         title="门店基本信息"
         layout="vertical"
         column={4}
-        extra={<Button type="primary">编辑</Button>}
-        labelStyle={{ fontSize: 10, color: '#999' }}
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              setEditable(!editable);
+            }}
+          >
+            编辑
+          </Button>
+        }
+        labelStyle={{ fontSize: 10, color: '#999', padding: '0 12px' }}
+        contentStyle={{ padding: '0 12px' }}
       >
-        <Descriptions.Item label="门店名称">LAN坞09</Descriptions.Item>
-        <Descriptions.Item label="门店编号">67064513</Descriptions.Item>
-        <Descriptions.Item label="门店类型">民宿</Descriptions.Item>
-        <Descriptions.Item label="门店地址">
-          上海/上海市/长宁区
-        </Descriptions.Item>
-        <Descriptions.Item label="详细地址" span={2}>
+        <DescItem label="门店名称">
+          {!editable ? (
+            'LAN坞09'
+          ) : (
+            <FormItem noStyle name="shopName">
+              <Input />
+            </FormItem>
+          )}
+        </DescItem>
+        <DescItem label="门店编号">67064513</DescItem>
+        <DescItem label="门店类型">民宿</DescItem>
+        <DescItem label="门店地址">上海/上海市/长宁区</DescItem>
+        <DescItem label="详细地址" span={2}>
           No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-        </Descriptions.Item>
-        <Descriptions.Item label="创建日期">2021-10-05</Descriptions.Item>
-        <Descriptions.Item label="负责人">xxx</Descriptions.Item>
-        <Descriptions.Item label="负责人手机号">13157215841</Descriptions.Item>
+        </DescItem>
+        <DescItem label="创建日期">2021-10-05</DescItem>
+        <DescItem label="负责人">xxx</DescItem>
+        <DescItem label="负责人手机号">13157215841</DescItem>
       </Descriptions>
-    </div>
+    </Form>
   );
 };
 
